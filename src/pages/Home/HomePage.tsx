a0127f2be6a1f17b973a396155cf25916b6ef99b
Hyeona01 <sha0209@dgu.ac.kr>
2024-10-17 17:16:04 +0900

    ✨ feat: Nav bar 컴포넌트 구현 #3

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,12 @@
+import Navbar from '@/components/Common/NavBar';
+
 const HomePage = () => {
-  return <>홈 페이지임</>;
+  return (
+    <>
+      홈 페이지임
+      <Navbar />
+    </>
+  );
 };
 
 export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        navbarGradient : 'linear-gradient(270deg, #FEFEFE 0.35%, #F4F4F9 175.32%)',
+      }
+    },
   },
   plugins: [],
 };
