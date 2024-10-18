c8b9a24ddf297fb10853f660769f63e2ccf5be2e
Hyeona01 <sha0209@dgu.ac.kr>
2024-10-18 11:33:57 +0900

    ✨ feat: button 컴포넌트 이미지 적용 및 props 분리 #3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ export default {
     extend: {
       backgroundImage:{
         navbarGradient : 'linear-gradient(270deg, #FEFEFE 0.35%, #F4F4F9 175.32%)',
+        applyBtn: "url('/applyButton.jpeg')",
+        grayGradient : 'linear-gradient(180deg, rgba(255, 255, 255, 0.80) 36.54%, #FFF 94.71%)'
       }
     },
   },
