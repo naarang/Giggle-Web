f259dbf6d50b3dd6a42b0046e76aa5d27fbe7b8e
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-19 00:07:50 +0900

    🍱 Add: 월 이동 아이콘 추가 및 연도 이동 기능 추가 #3

--- src/components/Common/DatePicker.tsx
+++ src/components/Common/DatePicker.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import moment from 'moment';
 import styles from '@/style/CustomCalendar.module.css';
+import NextMonthIcon from '@/assets/icons/Chevron.svg?react';
+import PrevMonthIcon from '@/assets/icons/ChevronLeft.svg?react';
 
 const months = [
   'January',
@@ -26,32 +28,9 @@ type Props = {
   setSelectedDate: (date: string) => void; // 선택된 날짜를 상위 컴포넌트에 전달하는 함수
 };
 
-const CustomCalendar = ({ setSelectedDate }: Props) => {
+const DatePicker = ({ setSelectedDate }: Props) => {
   // 현재 선택된 날짜 상태
   const [value, setValue] = useState<Value>(new Date());
-  // 현재 표시중인 월 상태
-  const [currentMonth, setCurrentMonth] = useState<string>(
-    moment().format('YYYY-MM'),
-  );
-
-  // 월이 변경될 때 호출되는 핸들러
-  const handleMonthChange = ({
-    activeStartDate,
-    view,
-  }: {
-    activeStartDate: Date | null;
-    view: string;
-  }) => {
-    if (view === 'month' && activeStartDate) {
-      console.log(
-        `Month changed to: ${moment(activeStartDate).format('MMMM YYYY')}`,
-      );
-      const newMonth = moment(activeStartDate).format('YYYY-MM');
-      if (newMonth !== currentMonth) {
-        setCurrentMonth(newMonth);
-      }
-    }
-  };
 
   // 달력 상단에 표시될 내비게이션 레이블 커스터마이징
   const navigationLabel = ({ date }: { date: Date }) => {
@@ -78,12 +57,12 @@ const CustomCalendar = ({ setSelectedDate }: Props) => {
           } // 요일 표시 형식 커스터마이징
           formatMonth={(locale, date) => (date.getMonth() + 1).toString()} // 월 표시 형식 커스터마이징
           className={styles.reactCalendar}
+          nextLabel={<NextMonthIcon />} // 월 이동 아이콘 커스텀
+          prevLabel={<PrevMonthIcon />} // 월 이동 아이콘 커스텀
+          //TODO: 연도 이동 아이콘 디자이넝에게 제공 받으면 추가해야 함.
           showNeighboringMonth={false} // 이전/다음 달의 날짜를 표시하지 않음
           locale="en-US" // 로케일 설정
           navigationLabel={navigationLabel} // 내비게이션 레이블 커스터마이징
-          next2Label={null} // 다음 해 버튼 숨김
-          prev2Label={null} // 이전 해 버튼 숨김
-          onActiveStartDateChange={handleMonthChange} // 활성 월 변경 시 호출되는 핸들러
         />
       </div>
     </div>
@@ -91,4 +70,4 @@ const CustomCalendar = ({ setSelectedDate }: Props) => {
 };
 
 // React.memo를 사용하여 불필요한 리렌더링 방지
-export default React.memo(CustomCalendar);
+export default React.memo(DatePicker);
