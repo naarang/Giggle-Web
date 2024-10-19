ebb27277f7a43331fb9713a8c955751175db1959
nakyong82 <nakyong7770@gmail.com>
2024-10-19 14:52:23 +0900

    ✨ feat: Tag 컴포넌트 구현하기 #3

---
+++ src/components/Common/Tag.tsx
@@ -0,0 +1,50 @@
+import TagCheckIcon from '@/assets/icons/TagCheckIcon';
+import TagDeleteIcon from '@/assets/icons/TagDeleteIcon';
+
+type TagProps = {
+  value: string; // 내용
+  padding: string; // padding style
+  isRounded: boolean; // 둥근 버튼 여부
+  onDelete?: (value: string) => void; // 삭제 버튼 클릭 시 호출될 함수
+  hasCheckIcon: boolean; // 왼쪽에 체크 아이콘 표시 여부
+  borderColor?: string;
+  backgroundColor: string;
+  color: string;
+  fontStyle: string; // typography type
+};
+
+const Tag = ({
+  value,
+  padding,
+  isRounded,
+  onDelete,
+  hasCheckIcon,
+  borderColor,
+  backgroundColor,
+  color,
+  fontStyle,
+}: TagProps) => {
+  return (
+    <div
+      className={`w-fit flex items-center gap-[0.5rem] ${fontStyle} ${isRounded ? 'rounded-[1.125rem]' : 'rounded-[0.375rem]'}`}
+      style={{
+        padding: padding,
+        backgroundColor: backgroundColor,
+        color: color,
+        borderColor: borderColor,
+        borderStyle: borderColor ? 'solid' : 'none',
+        borderWidth: '1px',
+      }}
+    >
+      {hasCheckIcon && <TagCheckIcon color={color} />}
+      {value}
+      {onDelete && (
+        <button onClick={() => onDelete(value)}>
+          <TagDeleteIcon color={color} />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Tag;
