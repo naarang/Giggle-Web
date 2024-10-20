3301f4f6bf0a289574e634a4ea3b1bb9ab84d53b
Hyeona01 <sha0209@dgu.ac.kr>
2024-10-21 00:18:58 +0900

    ✨ feat: onclick 핸들러 추가 및 디자인 변경사항 반영 #3

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,60 +1,64 @@
-import ApplyBtnImg from '@/assets/images/applyButton.jpeg';
+import CallIcon from '@/assets/icons/CallIcon.svg?react';
 /*
  *
- * type
- * - signIn
- * - smallRound
- * - smallSquare
+ * [ type ]
+ * - large
+ * - small
  * - applyNow
  * - back
  * - continue
  *
- * button color - css 코드로 전달받음
- * font color - css 코드로 전달받음
- *
- * TODO : ApplyNow 버튼 이미지 적용
- *
  */
 
-type buttonType = {
-  type: string;
-  bgColor: string | null;
-  fontColor: string | null;
-  borderColor: string | null;
-  title: string;
+type buttonProps = {
+  type: string; // 정의된 버튼을 5가지 타입으로 나누었습니다.
+  bgColor: string; // 버튼의 배경색
+  fontColor: string; // 버튼 글자색
+  isCallIcon?: boolean; // 전화기 아이콘 여부 (optional)
+  isBorder: boolean; // 버튼 테두리 여부
+  title: string; // 버튼에 포함되는 글자
+  onClick?: () => void; // 클릭 이벤트 핸들러 (optional)
 };
 
 const Button = ({
   type,
   bgColor,
   fontColor,
-  borderColor,
+  isCallIcon = false, // 기본값 false 설정
+  isBorder,
   title,
-}: buttonType) => {
+  onClick,
+}: buttonProps) => {
   const getButtonStyle = () => {
     switch (type) {
-      case 'signIn':
+      case 'large':
         return 'w-[327px] h-[52px] flex justify-center items-center rounded-[32px] text-[16px]';
-      case 'smallRound':
+      case 'small':
         return 'w-[89px] h-[40px] flex justify-center items-center rounded-[20px] text-[12px]';
-      case 'smallSquare':
-        return 'w-[89px] h-[36px] flex justify-center items-center rounded-[8px] text-[12px]';
       case 'applyNow':
         return `w-[327px] h-[52px] flex justify-center items-center rounded-[32px] bg-applyBtn bg-cover bg-center text-[16px] text-[#F4F4F9]`;
       case 'back':
         return 'w-[119px] h-[52px] flex justify-center items-center rounded-[32px] text-[16px]';
       case 'continue':
         return 'w-[200px] h-[52px] flex justify-center items-center rounded-[32px] text-[16px]';
-      default:
+      default: // 기본값으로 large type 적용
         return 'w-[327px] h-[52px] flex justify-center items-center rounded-[32px] text-[16px]';
     }
   };
 
   return (
     <button
-      className={`${getButtonStyle()} ${bgColor} ${fontColor} border-solid border-[0.5px] ${borderColor}`}
+      className={`${getButtonStyle()} ${bgColor} ${fontColor}  ${isBorder && 'border-solid border-[0.5px] border-[#1E1926]'}`}
+      onClick={onClick}
     >
-      {title}
+      {isCallIcon ? (
+        <div className="flex justify-center items-center gap-1.5">
+          <CallIcon />
+          <div className="text-semibold">{title}</div>
+        </div>
+      ) : (
+        <div className="text-semibold">{title}</div>
+      )}
     </button>
   );
 };
