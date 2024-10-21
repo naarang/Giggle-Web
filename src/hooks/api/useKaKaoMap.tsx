31911881adde398cd4a3aebd81733b92281b1ab1
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-21 19:53:41 +0900

    ✨ feat: 주소 검색 커스텀 훅 작성 #5

--- src/hooks/api/useKaKaoMap.tsx
+++ src/hooks/api/useKaKaoMap.tsx
@@ -1,6 +1,6 @@
-import { getGeoInfo } from '@/api/map';
-import { GeoPosition } from '@/types/api/map';
-import { useQuery } from '@tanstack/react-query';
+import { getGeoInfo, searchAddress } from '@/api/map';
+import { Document, GeoPosition, UseSearchAddressProps } from '@/types/api/map';
+import { useMutation, UseMutationResult, useQuery } from '@tanstack/react-query';
 import { Dispatch, SetStateAction } from 'react';
 
 export function useGetGeoInfo(
@@ -11,3 +11,17 @@ export function useGetGeoInfo(
     queryFn: () => getGeoInfo(setCurrentPosition),
   });
 }
+
+export const useSearchAddress = ({
+  onSuccess,
+  onError,
+}: UseSearchAddressProps): UseMutationResult<Document[], unknown, string, unknown> => {
+  return useMutation({
+    mutationFn: searchAddress,
+    onSuccess,
+    onError: (error) => {
+      console.error('주소 검색 중 오류 발생:', error);
+      onError?.(error);
+    },
+  });
+};
