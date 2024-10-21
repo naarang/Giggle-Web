de347ae5784cbff921a73565112148502e51dc2f
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-21 19:56:24 +0900

    ✨ feat: 객체 속성 중 일부만 선택해 추출하는 유틸리티 함수 작성 #5

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -1,9 +1,19 @@
-export const searchPlaces = (keyword: string) => {
-  const ps = new kakao.maps.services.Places();
-
-  ps.keywordSearch(keyword, (data, status) => {
-    if (status === kakao.maps.services.Status.OK) {
-      return data;
-    }
-  });
-};
+/**
+ * 객체에서 지정된 속성들만 선택하여 새 객체를 반환하는 함수
+ * @param obj 원본 객체
+ * @param keys 선택할 속성들의 배열
+ * @returns 선택된 속성들로 이루어진 새 객체
+ */
+export const pick = <T extends object, K extends keyof T>(
+  obj: T,
+  keys: K[],
+): Pick<T, K> =>
+  keys.reduce(
+    (result, key) => {
+      if (key in obj) {
+        result[key] = obj[key];
+      }
+      return result;
+    },
+    {} as Pick<T, K>,
+  );
