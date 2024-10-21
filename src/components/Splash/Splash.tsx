48ea5d61a897019740938b98691de68d39636b0d
Hyeona01 <sha0209@dgu.ac.kr>
2024-10-21 20:58:00 +0900

    ✨ feat: 스플래시 화면 작성 #17

---
+++ src/components/Splash/Splash.tsx
@@ -0,0 +1,11 @@
+import SplashIcon from '@/assets/icons/Splash.svg?react';
+
+const Splash = () => {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-[#FEF387] flex justify-center items-center">
+      <SplashIcon />
+    </div>
+  );
+};
+
+export default Splash;
