77b0bfc4e655b3533b156f3aff81dc98a989229d
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-22 16:29:37 +0900

    ✨ feat: 서류 관리 메인 페이지 레이아웃 작성 #24

---
+++ src/pages/ApplicationDocuments/ApplicationDocumentsPage.tsx
@@ -0,0 +1,53 @@
+import BottomButtonPanel from '@/components/Common/BottomButtonPanel';
+import Button from '@/components/Common/Button';
+import BaseHeader from '@/components/Common/Header/BaseHeader';
+import DocumentCardList from '@/components/Document/DocumentCardList';
+import { DocumentsSummaryResponse } from '@/types/api/document';
+
+const mockDocumentsSummaryResponse: DocumentsSummaryResponse = {
+    part_time_employment_permits: {
+      id: 1001,
+      pdf_url: "https://example.com/permits/part_time_1001.pdf",
+      hwp_url: "https://example.com/permits/part_time_1001.hwp",
+      word_url: "https://example.com/permits/part_time_1001.docx",
+      status: "SUBMITTED"
+    },
+    standard_labor_contract: {
+      id: 2001,
+      pdf_url: "https://example.com/contracts/standard_2001.pdf",
+      hwp_url: "https://example.com/contracts/standard_2001.hwp",
+      word_url: "https://example.com/contracts/standard_2001.docx",
+      status: "CONFIRMATION"
+    },
+
+};
+
+const ApplicationDocumentsPage = () => {
+      {/*integrated_application: {
+      id: 3001,
+      pdf_url: "https://example.com/applications/integrated_3001.pdf",
+      hwp_url: "https://example.com/applications/integrated_3001.hwp",
+      word_url: "https://example.com/applications/integrated_3001.docx"
+    }*/}
+  return (
+    <div>
+      <BaseHeader
+        hasBackButton={true}
+        hasMenuButton={true}
+        title="Application Documents"
+      />
+      <DocumentCardList documents={mockDocumentsSummaryResponse} />
+      <BottomButtonPanel>
+        <Button
+          type="large"
+          bgColor="bg-[#F4F4F9]"
+          fontColor="text-[#bdbdbd]"
+          isBorder={false}
+          title="Next"
+        />
+      </BottomButtonPanel>
+    </div>
+  );
+};
+
+export default ApplicationDocumentsPage;
