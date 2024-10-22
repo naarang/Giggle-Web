e4cf7c40caa3573110a32ccb42047c7a639cec2a
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-22 20:19:41 +0900

    :sparkles: feat: dispenser 형태로 변경 #24

--- src/components/Document/DocumentCardList.tsx
+++ src/components/Document/DocumentCardList.tsx
@@ -3,9 +3,9 @@ import {
   DocumentType,
   DocumentTypeInfo,
 } from '@/types/api/document';
-import DocumentCard from './DocumentCard';
 import AddIcon from '@/assets/icons/AddButton.svg?react';
 import { useNavigate } from 'react-router-dom';
+import DocumentCardDispenser from './DocumentCard';
 
 const DocumentCardList = ({
   documents,
@@ -14,6 +14,11 @@ const DocumentCardList = ({
 }) => {
   const navigate = useNavigate();
   const documentTypes = Object.values(DocumentType);
+  const handleOnNext = () => {
+    //추후 api 작성 시 완성
+    alert('onNext');
+  };
+
   const MakeDocumentButton = ({ title }: { title: string }) => {
     return (
       <div className="w-full relative rounded-[1.125rem] bg-white border border-[#dcdcdc] flex flex-col items-start justify-start py-6 cursor-pointer text-left text-[#1e1926]">
@@ -32,10 +37,14 @@ const DocumentCardList = ({
   };
 
   return (
-    <div className="w-full px-6">
+    <div className="flex flex-col w-full px-6 gap-2">
       {documentTypes.map((property) =>
         documents[property] ? (
-          <DocumentCard document={documents[property]} />
+          <DocumentCardDispenser
+            document={documents[property]}
+            title={DocumentTypeInfo[property].name}
+            onNext={handleOnNext}
+          />
         ) : (
           <MakeDocumentButton title={DocumentTypeInfo[property].name} />
         ),
