df7497f4497e936b276cf4ff04efad7c1d045afe
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-23 15:54:50 +0900

    ✨ feat: SubHeader 내의 문구를 위한 name, content Map 작성 #31

--- src/constants/documents.ts
+++ src/constants/documents.ts
@@ -15,3 +15,24 @@ export const DocumentTypeInfo = {
     key: 'integrated_application',
   },
 } as const;
+
+export const DocumentSubTitleContent = {
+  [DocumentType.PART_TIME_PERMIT]: {
+    name: 'Part-Time Employment Permit',
+    key: 'part_time_employment_permits',
+    content:
+      "International students must have the applicable permit to work part-time or part-time. If you don't get permission, you can become an illegal resident.",
+  },
+  [DocumentType.LABOR_CONTRACT]: {
+    name: 'Standard labor contract for short-time workers',
+    key: 'standard_labor_contract',
+    content:
+      'Regardless of the type of employment, all workers are required to write an employment contract, which allows them to maintain a legal and transparent employment relationship.',
+  },
+  [DocumentType.INTEGRATED_APPLICATION]: {
+    name: 'Application Form',
+    key: 'integrated_application',
+    content:
+      'The Application Form is a required document that international students must submit to legally work part-time in Korea. This form reports the workplace and working conditions, allowing the student to obtain employment approval.',
+  },
+} as const;
