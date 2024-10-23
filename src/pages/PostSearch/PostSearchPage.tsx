b7e3a238f26a62e4f3b7cd8eb62d3d2701670fac
nakyong82 <nakyong7770@gmail.com>
2024-10-23 14:25:53 +0900

    🐛 fix: 공고 검색 페이지에서 뒤로 가기 시 메인 페이지로 이동하기 #15

--- src/pages/PostSearch/PostSearchPage.tsx
+++ src/pages/PostSearch/PostSearchPage.tsx
@@ -1,13 +1,15 @@
 import TextFieldHeader from '@/components/Common/Header/TextFieldHeader';
 import PostSearchFilterList from '@/components/PostSearch/PostSearchFilterList';
 import PostSearchResult from '@/components/PostSearch/PostSearchResult';
+import { useNavigate } from 'react-router-dom';
 
 const PostSearchPage = () => {
+  const navigate = useNavigate();
   // TODO: 여기서 검색어, 검색 필터 모두 전역변수로 관리하기
   return (
     <>
       <TextFieldHeader
-        onClickBackButton={() => console.log('?')}
+        onClickBackButton={() => navigate('/')}
         onClickSearchButton={(value) => console.log(value)}
         placeholder="Search for job posting name"
       />
