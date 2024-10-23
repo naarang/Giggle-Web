a40a4391f5b9169fc1fc9e71b34cc46df8c35f3d
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-23 16:04:40 +0900

    ✨ feat: 문서 작성 페이지 공통 레이아웃 구성 #31

--- src/pages/WriteDocuments/WriteDocumentsPage.tsx
+++ src/pages/WriteDocuments/WriteDocumentsPage.tsx
@@ -1,7 +1,27 @@
-const WriteDocumentsPage = () => {
-  return (
-    <div>WriteDocumentsPage</div>
-  )
-}
+import BottomButtonPanel from '@/components/Common/BottomButtonPanel';
+import Button from '@/components/Common/Button';
+import BaseHeader from '@/components/Common/Header/BaseHeader';
+import DocumentSubHeader from '@/components/Document/write/DocumentSubHeader';
+import DocumentFormDispenser from '@/components/WriteDocuments/DocumentFormDispenser';
+import { DocumentType } from '@/types/api/document';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export default WriteDocumentsPage
+const WriteDocumentsPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { type } = location.state || {};
+  return (
+    <div>
+      <BaseHeader
+        hasBackButton={true}
+        hasMenuButton={true}
+        title="Fill in document"
+        onClickBackButton={() => navigate('/application-documents')}
+      />
+      <DocumentSubHeader type={type as DocumentType} />
+      <DocumentFormDispenser type={type as DocumentType} isEdit={false} />
+    </div>
+  );
+};
+
+export default WriteDocumentsPage;
