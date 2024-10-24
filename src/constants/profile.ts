82e4565fd01c45cf464d0dfb32bf13cb6370b477
naarang <nakyong07@naver.com>
2024-10-24 15:01:34 +0000

    ✨ feat: edit 페이지 get/patch 타입 정의 #28

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -6,3 +6,30 @@ export const enum IconType {
   LANGUAGE = 'LANGUAGE',
   LOGOUT = 'LOGOUT',
 }
+
+export const enum GenderType {
+  MALE='Maile',
+  FEMALE='Femail',
+  NONE='None',
+}
+
+export const enum VisaType{
+  D_2_1='D-2-1',
+  D_2_2='D-2-2',
+  D_2_3='D-2-3',
+  D_2_4='D-2-4',
+  D_2_6='D-2-6',
+  D_2_7='D-2-7',
+  D_2_8='D-2-8',
+  D_4_1='D-4-1',
+  D_4_7='D-4-7',
+  F_2='F-2'
+}
+
+export const enum NationalityType{
+  SOUTH_KOREA= 'South Korea',
+  JAPAN= 'Japan',
+  CHINA= 'China',
+  VIETNAME= 'Vietname',
+  UZBEKISTAN= 'Uzbekistan',
+}
--- src/pages/Profile/EditProfilePage.tsx
+++ src/pages/Profile/EditProfilePage.tsx
@@ -1,25 +1,35 @@
 import Button from '@/components/Common/Button';
+import Dropdown from '@/components/Common/Dropdown';
 import BaseHeader from '@/components/Common/Header/BaseHeader';
 import Input from '@/components/Common/Input';
+import RadioButton from '@/components/Information/RadioButton';
 import EditProfilePicture from '@/components/Profile/EditProfilePicture';
 import { buttonTypeKeys } from '@/constants/components';
-import { UserEditProfileDataType } from '@/types/api/profile';
+import { GenderType, NationalityType, VisaType } from '@/constants/profile';
+import { UserProfileDetailDataType } from '@/types/api/profile';
 import { InputType } from '@/types/common/input';
+import { transformToEditProfileData } from '@/utils/editProfileData';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { country, phone, visa } from '@/constants/information';
 
 const EditProfilePage = () => {
   const navigate = useNavigate();
-  const [userData, setUserData] = useState<UserEditProfileDataType | undefined>(
-    undefined,
-  );
+  const [userData, setUserData] = useState<
+    UserProfileDetailDataType | undefined
+  >(undefined);
   const [profileImage, setProfileImage] = useState<File | null>(null);
+  const [phoneNum, setPhoneNum] = useState({
+    start: '',
+    middle: '',
+    end: '',
+  });
 
   const handleInputChange =
-    (field: keyof UserEditProfileDataType) => (value: string) => {
+    (field: keyof UserProfileDetailDataType) => (value: string) => {
       setUserData((prevData) => {
         if (!prevData) {
-          return { [field]: value } as UserEditProfileDataType;
+          return { [field]: value } as UserProfileDetailDataType;
         }
         return {
           ...prevData,
@@ -36,24 +46,69 @@ const EditProfilePage = () => {
     }
   };
 
-  const handleSubmit = () => {
-    // API - 3.5 (유학생) 프로필 수정
-    console.log('file 변경 - ' + profileImage);
+  const handleSubmit = async (): Promise<void> => {
+    // TODO : API - 3.5 (유학생) 프로필 수정
+    if (!userData) return;
 
     navigate('/profile');
+
+    // get -> patch 데이터 변환
+    const transformedData = transformToEditProfileData(
+      userData,
+      phoneNum,
+      profileImage,
+      Boolean(profileImage), // 이미지 변경 여부 확인
+    );
+    // console.log('transformedData: ', transformedData);
+    try {
+      const formData = new FormData();
+
+      // 이미지가 있을 경우 FormData에 추가
+      if (transformedData.image) {
+        formData.append('image', transformedData.image);
+      }
+
+      // JSON 데이터를 Blob으로 변환 후 FormData에 추가
+      formData.append(
+        'body',
+        new Blob([JSON.stringify(transformedData.body)], {
+          type: 'application/json',
+        }),
+      );
+      /*
+      const response = await axios.patch('/api/v1/users', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log('API 성공:', response.data);
+      */
+      navigate('/profile');
+    } catch (error) {
+      console.error('API 호출 중 에러 발생:', error);
+    }
   };
 
+  // 전화번호를 3개의 파트로 구분
   useEffect(() => {
+    if (userData?.phone_number) {
+      const [start, middle, end] = userData.phone_number.split('-');
+      setPhoneNum({ start, middle, end });
+    }
+  }, [userData]);
+
+  useEffect(() => {
+    // TODO : API - 3.1 (유학생) 유저 프로필 조회하기
     setUserData({
       profile_img_url:
         'https://images.pexels.com/photos/1458926/pexels-photo-1458926.jpeg?cs=srgb&dl=pexels-poodles2doodles-1458926.jpg&fm=jpg',
       first_name: 'Hyeona',
       last_name: 'Seol',
-      birth: '0000.00.00',
-      gender: 'Female',
-      nationality: 'Korea',
-      visa_satus: 'D_2_7',
-      telephone_number: '010-1111-2222',
+      birth: '2001-02-09',
+      gender: GenderType.FEMALE,
+      nationality: NationalityType.SOUTH_KOREA,
+      visa: VisaType.D_2_1,
+      phone_number: '010-1111-2222',
     });
   }, []);
 
@@ -72,65 +127,155 @@ const EditProfilePage = () => {
               profileImgUrl={userData.profile_img_url}
               onImageUpdate={setProfileImage}
             />
-            <Input
-              inputType={InputType.TEXT}
-              placeholder="First Name"
-              value={userData.first_name}
-              onChange={handleInputChange('first_name')}
-              canDelete={false}
-            />
-            <Input
-              inputType={InputType.TEXT}
-              placeholder="Last Name"
-              value={userData.last_name}
-              onChange={handleInputChange('last_name')}
-              canDelete={false}
-            />
-            <Input
-              inputType={InputType.TEXT}
-              placeholder="Birth"
-              value={userData.birth}
-              onChange={handleInputChange('birth')}
-              canDelete={false}
-            />
-            <Input
-              inputType={InputType.TEXT}
-              placeholder="Gender"
-              value={userData.gender}
-              onChange={handleInputChange('gender')}
-              canDelete={false}
-            />
-            <Input
-              inputType={InputType.TEXT}
-              placeholder="Nationality"
-              value={userData.nationality}
-              onChange={handleInputChange('nationality')}
-              canDelete={false}
-            />
-            <Input
-              inputType={InputType.TEXT}
-              placeholder="Visa Status"
-              value={userData.visa_satus}
-              onChange={handleInputChange('visa_satus')}
-              canDelete={false}
-            />
-            <Input
-              inputType={InputType.TEXT}
-              placeholder="Telephone Number"
-              value={userData.telephone_number}
-              onChange={handleInputChange('telephone_number')}
-              canDelete={false}
-            />
-            <div className="pt-3 pb-[3.125rem] flex justify-center items-center">
-              <Button
-                type={buttonTypeKeys.LARGE}
-                title="Save"
-                bgColor="bg-[#FEF387]"
-                fontColor="text-[#1E1926]"
-                onClick={handleSubmit}
-                isBorder={false}
+
+            <div className="w-full">
+              {/* 이름 작성 */}
+              <div className="w-full flex items-center justify-start body-3 color-[#222] px-[0.25rem] py-[0.375rem]">
+                First Name
+              </div>
+              <Input
+                inputType={InputType.TEXT}
+                placeholder="First Name"
+                value={userData.first_name}
+                onChange={handleInputChange('first_name')}
+                canDelete={false}
               />
             </div>
+            {/* 성 작성 */}
+            <div className="w-full">
+              <div className="w-full flex items-center justify-start body-3 color-[#222] px-[0.25rem] py-[0.375rem]">
+                Last Name
+              </div>
+              <Input
+                inputType={InputType.TEXT}
+                placeholder="Last Name"
+                value={userData.last_name}
+                onChange={handleInputChange('last_name')}
+                canDelete={false}
+              />
+            </div>
+            <div className="w-full">
+              <div className="w-full flex items-center justify-start body-3 color-[#222] px-[0.25rem] py-[0.375rem]">
+                Gender
+              </div>
+              <div className="w-full flex flex-row gap-[1.75rem]">
+                <RadioButton
+                  value={GenderType.FEMALE as string}
+                  setValue={(value: string) =>
+                    setUserData({
+                      ...userData,
+                      gender: value as GenderType,
+                    })
+                  }
+                  isOn={userData.gender === GenderType.FEMALE}
+                />
+                <RadioButton
+                  value={GenderType.MALE as string}
+                  setValue={(value: string) =>
+                    setUserData({
+                      ...userData,
+                      gender: value as GenderType,
+                    })
+                  }
+                  isOn={userData.gender === GenderType.MALE}
+                />
+                <RadioButton
+                  value={GenderType.NONE as string}
+                  setValue={(value: string) =>
+                    setUserData({
+                      ...userData,
+                      gender: value as GenderType,
+                    })
+                  }
+                  isOn={userData.gender === GenderType.NONE}
+                />
+              </div>
+            </div>
+            {/* 생년월일 선택 */}
+            <div className="w-full">
+              <div className="w-full flex items-center justify-start body-3 color-[#222] px-[0.25rem] py-[0.375rem]">
+                Date of birth
+              </div>
+              <Dropdown
+                value={userData.birth.replace(/-/g, '/')}
+                placeholder="Select Date"
+                options={[]}
+                isCalendar={true}
+                setValue={(value) => setUserData({ ...userData, birth: value })}
+              />
+            </div>
+            {/* 국적 선택 */}
+            <div className="w-full">
+              <div className="w-full flex items-center justify-start body-3 color-[#222] px-[0.25rem] py-[0.375rem]">
+                Nationality
+              </div>
+              <Dropdown
+                value={userData.nationality}
+                placeholder="Select Nationality"
+                options={country}
+                setValue={(value: string) =>
+                  setUserData({ ...userData, nationality: value })
+                }
+              />
+            </div>
+            {/* 비자 선택 */}
+            <div className="w-full">
+              <div className="w-full flex items-center justify-start body-3 color-[#222] px-[0.25rem] py-[0.375rem]">
+                Visa Status
+              </div>
+              <Dropdown
+                value={userData.visa}
+                placeholder="Select Visa Status"
+                options={visa}
+                setValue={(value: string) =>
+                  setUserData({ ...userData, visa: value as VisaType })
+                }
+              />
+            </div>
+            {/* 전화번호 선택, dropdown으로 앞 번호를, 중간 번호와 뒷 번호는 각각 input으로 입력 받음 */}
+            <div className="w-full">
+              <div className="w-full flex flex-row items-center justify-start body-3 color-[#222] px-[0.25rem] py-[0.375rem]">
+                Telephone No.
+              </div>
+              <div className="w-full flex flex-row gap-2 justify-between">
+                <div className="w-full h-[2.75rem]">
+                  <Dropdown
+                    value={phoneNum.start}
+                    placeholder="+82"
+                    options={phone}
+                    setValue={(value) =>
+                      setPhoneNum({ ...phoneNum, start: value })
+                    }
+                  />
+                </div>
+                <Input
+                  inputType={InputType.TEXT}
+                  placeholder="0000"
+                  value={phoneNum.middle}
+                  onChange={(value) =>
+                    setPhoneNum({ ...phoneNum, middle: value })
+                  }
+                  canDelete={false}
+                />
+                <Input
+                  inputType={InputType.TEXT}
+                  placeholder="0000"
+                  value={phoneNum.end}
+                  onChange={(value) => setPhoneNum({ ...phoneNum, end: value })}
+                  canDelete={false}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-32 pt-3 pb-[3.125rem] flex justify-center items-center">
+            <Button
+              type={buttonTypeKeys.LARGE}
+              title="Save"
+              bgColor="bg-[#FEF387]"
+              fontColor="text-[#1E1926]"
+              onClick={handleSubmit}
+              isBorder={false}
+            />
           </div>
         </div>
       ) : (
