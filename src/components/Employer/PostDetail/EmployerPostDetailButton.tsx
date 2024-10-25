a5c7bd92a02b9a1ebcd4beeb133385617c07f879
naarang <nakyong07@naver.com>
2024-10-25 15:33:57 +0000

    ✨feat: 고용주 공고 상세 페이지 구현하기 #48

---
+++ src/components/Employer/PostDetail/EmployerPostDetailButton.tsx
@@ -0,0 +1,37 @@
+import Button from '@/components/Common/Button';
+import { buttonTypeKeys } from '@/constants/components';
+import { useState } from 'react';
+import EmployerPostDeleteBottomSheet from '@/components/Employer/PostDetail/EmployerPostDeleteBottomSheet';
+
+const EmployerPostDetailButton = () => {
+  const [isOpenDeleteBottomSheet, setIsOpenDeleteBottomSheet] =
+    useState<boolean>(false);
+  return (
+    <>
+      <footer className="fixed bottom-0 left-0 w-full flex gap-[0.5rem] pt-[0.75rem] px-[1.5rem] pb-[2.5rem] bg-white z-20">
+        <Button
+          type={buttonTypeKeys.BACK}
+          bgColor="bg-[#F4F4F9]"
+          fontColor="text-[#BDBDBD]"
+          isBorder={false}
+          title="Delete"
+          onClick={() => setIsOpenDeleteBottomSheet(true)}
+        />
+        <Button
+          type={buttonTypeKeys.CONTINUE}
+          bgColor="bg-[#FEF387]"
+          fontColor="text-[#1E1926]"
+          isBorder={false}
+          title="Edit"
+          // TODO: 고용주 - 공고 수정 - 1단계로 이동하기
+        />
+      </footer>
+      <EmployerPostDeleteBottomSheet
+        isShowBottomsheet={isOpenDeleteBottomSheet}
+        setIsShowBottomSheet={setIsOpenDeleteBottomSheet}
+      />
+    </>
+  );
+};
+
+export default EmployerPostDetailButton;
--- src/pages/Employer/PostDetail/EmployerPostDetailPage.tsx
+++ src/pages/Employer/PostDetail/EmployerPostDetailPage.tsx
@@ -1,19 +1,19 @@
 import BaseHeader from '@/components/Common/Header/BaseHeader';
-import PostDetailApplyButton from '@/components/PostDetail/PostDetailApplyButton';
+import EmployerPostDetailButton from '@/components/Employer/PostDetail/EmployerPostDetailButton';
 import PostDetailCompanyImageList from '@/components/PostDetail/PostDetailCompanyImageList';
 import PostDetailContent from '@/components/PostDetail/PostDetailContent';
 import PostDetailTitle from '@/components/PostDetail/PostDetailTitle';
 import { POST_DETAIL_DATA } from '@/constants/postDetail';
 import { useNavigate } from 'react-router-dom';
 
-const PostDetailPage = () => {
+const EmployerPostDetailPage = () => {
   const navigate = useNavigate();
 
   return (
     <>
       <BaseHeader
         hasBackButton={true}
-        onClickBackButton={() => navigate('/search')}
+        onClickBackButton={() => navigate(-1)}
         hasMenuButton={false}
         title="Detail"
       />
@@ -22,9 +22,9 @@ const PostDetailPage = () => {
       />
       <PostDetailTitle postDetailData={POST_DETAIL_DATA} />
       <PostDetailContent postDetailData={POST_DETAIL_DATA} />
-      <PostDetailApplyButton />
+      <EmployerPostDetailButton />
     </>
   );
 };
 
-export default PostDetailPage;
+export default EmployerPostDetailPage;
