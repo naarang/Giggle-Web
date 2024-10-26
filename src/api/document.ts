d8a95d7d32e7971ffd285af5a8e9b499ff045835
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-26 17:23:05 +0900

    :sparkles: feat: 문서별 수정 api 통신 함수, 훅 작성 #54

--- src/api/document.ts
+++ src/api/document.ts
@@ -5,6 +5,7 @@ import {
   SearchSchoolResponse,
 } from '@/types/api/document';
 import { api } from '.';
+import { PartTimePermitFormRequest } from '../types/api/document';
 
 //시간제 취업허가서 작성 api 통신 함수
 export const postPartTimeEmployPermit = async ({
@@ -21,7 +22,22 @@ export const postPartTimeEmployPermit = async ({
   return response.data;
 };
 
-//표준 근로계약서 작성 api 통신 함수
+// 8.10 (유학생) 시간제 취업허가서 수정하기
+export const putPartTimeEmployPermit = async ({
+  id,
+  document,
+}: {
+  id: number;
+  document: PartTimePermitFormRequest;
+}): Promise<{ success: boolean }> => {
+  const response = await api.put(
+    `/users/documents/${id}/part-time-employment-permits`,
+    document,
+  );
+  return response.data;
+};
+
+// 8.6 표준 근로계약서 작성 api 통신 함수
 export const postStandardLaborContracts = async ({
   id,
   document,
@@ -36,7 +52,22 @@ export const postStandardLaborContracts = async ({
   return response.data;
 };
 
-// 8.8 표준 근로계약서 작성 api 통신 함수
+// 8.12 (유학생) 근로계약서 수정하기
+export const putStandardLaborContracts = async ({
+  id,
+  document,
+}: {
+  id: number;
+  document: LaborContractEmployeeInfo;
+}): Promise<{ success: boolean }> => {
+  const response = await api.put(
+    `/users/documents/${id}/standard-labor-contracts`,
+    document,
+  );
+  return response.data;
+};
+
+// 8.8 통합신청서 작성 api 통신 함수
 export const postIntegratedApplications = async ({
   id,
   document,
@@ -46,6 +77,21 @@ export const postIntegratedApplications = async ({
 }): Promise<{ id: number }> => {
   const response = await api.post(
     `/users/user-owner-job-postings/${id}/documents/integrated_applications`,
+    document,
+  );
+  return response.data;
+};
+
+// 8.14 통합신청서 수정 api 통신 함수
+export const putIntegratedApplications = async ({
+  id,
+  document,
+}: {
+  id: number;
+  document: IntegratedApplicationData;
+}): Promise<{ success: boolean }> => {
+  const response = await api.post(
+    `/users/documents/${id}/integrated-applications`,
     document,
   );
   return response.data;
--- src/hooks/api/useDocument.ts
+++ src/hooks/api/useDocument.ts
@@ -2,6 +2,9 @@ import {
   postIntegratedApplications,
   postPartTimeEmployPermit,
   postStandardLaborContracts,
+  putIntegratedApplications,
+  putPartTimeEmployPermit,
+  putStandardLaborContracts,
   searchSchool,
 } from '@/api/document';
 import { DocumentType, SearchSchoolResponse } from '@/types/api/document';
@@ -13,6 +16,23 @@ export const usePostPartTimeEmployPermit = () => {
   const navigate = useNavigate();
   return useMutation({
     mutationFn: postPartTimeEmployPermit,
+    onSuccess: () => {
+      navigate('/');
+    },
+    onError: () =>
+      navigate('/write-documents', {
+        state: {
+          type: DocumentType.PART_TIME_PERMIT,
+        },
+      }),
+  });
+};
+
+// 8.10 (유학생)시간제취업허가서 수정 api 통신 커스텀 훅
+export const usePutPartTimeEmployPermit = () => {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: putPartTimeEmployPermit,
     onSuccess: () => {
       navigate('/');
     },
@@ -42,11 +62,45 @@ export const usePostStandardLaborContracts = () => {
   });
 };
 
+// 8.12 (유학생) 표준 근로계약서 수정 api 통신 커스텀 훅
+export const usePutStandardLaborContracts = () => {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: putStandardLaborContracts,
+    onSuccess: () => {
+      navigate('/');
+    },
+    onError: () =>
+      navigate('/write-documents', {
+        state: {
+          type: DocumentType.LABOR_CONTRACT,
+        },
+      }),
+  });
+};
+
 // 8.8 통합신청서 생성 api 통신 커스텀 훅
 export const usePostIntegratedApplicants = () => {
   const navigate = useNavigate();
   return useMutation({
     mutationFn: postIntegratedApplications,
+    onSuccess: () => {
+      navigate('/');
+    },
+    onError: () =>
+      navigate('/write-documents', {
+        state: {
+          type: DocumentType.INTEGRATED_APPLICATION,
+        },
+      }),
+  });
+};
+
+// 8.14 (유학생) 통합신청서 수정 api 통신 커스텀 훅
+export const usePutIntegratedApplicants = () => {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: putIntegratedApplications,
     onSuccess: () => {
       navigate('/');
     },
