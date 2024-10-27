bbeea3576e544bea69d883c19fd63cc64e4ec5a7
nakyong82 <nakyong7770@gmail.com>
2024-10-28 03:15:26 +0900

    ✨feat: 고용주 메인 페이지 구현하기 및 메인 페이지에서 페이지 이동들 연결하기 #61

--- src/store/postSearch.ts
+++ src/store/postSearch.ts
@@ -1,17 +1,21 @@
-import { initialFilterList } from '@/constants/postSearch';
+import { initialFilterList, POST_SEARCH_MENU } from '@/constants/postSearch';
 import { PostSearchFilterItemType } from '@/types/PostSearchFilter/PostSearchFilterItem';
 import { create } from 'zustand';
 
 type PostSearchStore = {
+  sortType: POST_SEARCH_MENU;
   searchText: string;
   filterList: PostSearchFilterItemType;
+  updateSortType: (type: POST_SEARCH_MENU) => void;
   updateSearchText: (text: string) => void;
   updateFilterList: (newFilterList: PostSearchFilterItemType) => void;
 };
 
 export const usePostSearchStore = create<PostSearchStore>()((set) => ({
+  sortType: POST_SEARCH_MENU.POPULAR,
   searchText: '',
   filterList: initialFilterList,
+  updateSortType: (type) => set(() => ({ sortType: type })),
   updateSearchText: (text) => set(() => ({ searchText: text })),
   updateFilterList: (newFilterList) =>
     set(() => ({ filterList: newFilterList })),
