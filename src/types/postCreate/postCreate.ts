467593ac1664bce61c4c2dcd1670b71fcc6915ff
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-29 04:56:02 +0900

    :wrench: chore: 공고 관련 타입, state 정의 #68

---
+++ src/types/postCreate/postCreate.ts
@@ -0,0 +1,144 @@
+// Enums
+export enum JobCategory {
+  GENERAL_INTERPRETATION_TRANSLATION = 'GENERAL_INTERPRETATION_TRANSLATION',
+  FOOD_SERVICE_ASSISTANT = 'FOOD_SERVICE_ASSISTANT',
+  GENERAL_ADMINISTRATIVE_SUPPORT = 'GENERAL_ADMINISTRATIVE_SUPPORT',
+  ENGLISH_KIDS_CAFE = 'ENGLISH_KIDS_CAFE',
+  GENERAL_CAFE = 'GENERAL_CAFE',
+  PART_TIME_WORK = 'PART_TIME_WORK',
+  TOUR_GUIDE_AND_DUTY_FREE_ASSISTANT = 'TOUR_GUIDE_AND_DUTY_FREE_ASSISTANT',
+  MANUFACTURING = 'MANUFACTURING',
+}
+
+export enum DayOfWeek {
+  WEEKDAYS = 'WEEKDAYS',
+  WEEKEND = 'WEEKEND',
+  MONDAY = 'MONDAY',
+  TUESDAY = 'TUESDAY',
+  WEDNESDAY = 'WEDNESDAY',
+  THURSDAY = 'THURSDAY',
+  FRIDAY = 'FRIDAY',
+  SATURDAY = 'SATURDAY',
+  SUNDAY = 'SUNDAY',
+  NEGOTIABLE = 'NEGOTIABLE',
+}
+
+export enum WorkPeriod {
+  ONE_DAY = 'ONE_DAY',
+  LESS_THAN_ONE_WEEK = 'LESS_THAN_ONE_WEEK',
+  ONE_WEEK_TO_ONE_MONTH = 'ONE_WEEK_TO_ONE_MONTH',
+  ONE_MONTH_TO_THREE_MONTHS = 'ONE_MONTH_TO_THREE_MONTHS',
+  THREE_MONTHS_TO_SIX_MONTHS = 'THREE_MONTHS_TO_SIX_MONTHS',
+  SIX_MONTHS_TO_ONE_YEAR = 'SIX_MONTHS_TO_ONE_YEAR',
+  MORE_THAN_ONE_YEAR = 'MORE_THAN_ONE_YEAR',
+}
+
+export enum EmploymentType {
+  PARTTIME = 'PARTTIME',
+  INTERNSHIP = 'INTERNSHIP',
+}
+
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+  NONE = 'NONE',
+}
+
+export enum EducationLevel {
+  BACHELOR = 'BACHELOR',
+  ASSOCIATE = 'ASSOCIATE',
+  HIGHSCHOOL = 'HIGHSCHOOL',
+}
+
+export enum Visa {
+  D_2_1 = 'D_2_1',
+  D_2_2 = 'D_2_2',
+  D_2_3 = 'D_2_3',
+  D_2_4 = 'D_2_4',
+  D_2_6 = 'D_2_6',
+  D_2_7 = 'D_2_7',
+  D_2_8 = 'D_2_8',
+  D_4_1 = 'D_4_1',
+  D_4_7 = 'D_4_7',
+  F_2 = 'F_2',
+}
+
+// Types
+export type WorkDayTime = {
+  day_of_week: DayOfWeek;
+  work_start_time?: string;
+  work_end_time?: string;
+};
+
+export type Address = {
+  address_name: string;
+  region_1depth_name: string;
+  region_2depth_name: string;
+  region_3depth_name: string;
+  region_4depth_name?: string;
+  address_detail: string;
+  longitude: number;
+  latitude: number;
+};
+
+export type JobPostingForm = {
+  images: File[];
+  body: {
+    title: string;
+    job_category: JobCategory;
+    work_day_times: WorkDayTime[];
+    work_period: WorkPeriod;
+    hourly_rate: number;
+    employment_type: EmploymentType;
+    address: Address;
+    recruitment_dead_line?: string;
+    recruitment_number: number;
+    gender: Gender;
+    age_restriction: number;
+    education_level: EducationLevel;
+    visa: Visa;
+    recruiter_name: string;
+    recruiter_email: string;
+    recruiter_phone_number: string;
+    description: string;
+    preferred_conditions: string;
+  };
+};
+
+// Initial State
+export const initialJobPostingState: JobPostingForm = {
+  images: [],
+  title: '',
+  job_category: JobCategory.GENERAL_INTERPRETATION_TRANSLATION,
+  work_day_times: [
+    {
+      day_of_week: DayOfWeek.NEGOTIABLE,
+      work_start_time: '',
+      work_end_time: '',
+    },
+  ],
+  work_period: WorkPeriod.ONE_MONTH_TO_THREE_MONTHS,
+  hourly_rate: 0,
+  employment_type: EmploymentType.PARTTIME,
+  address: {
+    address_name: '',
+    region_1depth_name: '',
+    region_2depth_name: '',
+    region_3depth_name: '',
+    region_4depth_name: '',
+    address_detail: '',
+    longitude: 0,
+    latitude: 0,
+  },
+  recruitment_dead_line: '',
+  recruitment_number: 1,
+  gender: Gender.NONE,
+  age_restriction: 0,
+  education_level: EducationLevel.HIGHSCHOOL,
+  visa: Visa.D_2_1,
+  recruiter_name: '',
+  recruiter_email: '',
+  recruiter_phone_number: '',
+  description: '',
+  preferred_conditions: '',
+};
