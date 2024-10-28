b67837674b898c6d8a7ad6fbd30451a69a24b444
Hyeona01 <sha0209@dgu.ac.kr>
2024-10-28 23:16:21 +0900

    ♻️ refactor: api 배치 순서 변경 #42

--- src/api/resumes.ts
+++ src/api/resumes.ts
@@ -13,23 +13,23 @@ export const getResume = async () => {
   return response.data;
 };
 
-// 7.8 (유학생) 자기소개 수정하기
-export const patchIntroduction = async (introduction: IntroDuctionRequest) => {
-  const response = await api.patch('/users/resumes/introduction', introduction);
-  return response.data;
-};
-
-// 7.15 (유학생) 자기소개 삭제하기
-export const deleteIntroduction = async () => {
-  const response = await api.delete('/users/resumes/introduction');
-  return response.data;
-};
-
 // 7.2 경력 상세 조회하기
 export const getWorkExperience = async (id: number) => {
   const response = await api.get(
     `/users/resumes/work-experiences/${id}/details`,
   );
+  return response.data;
+};
+
+// 7.3 학력 상세 조회하기
+export const getEducation = async (id: number) => {
+  const response = await api.get(`/users/resumes/educations/${id}/details`);
+  return response.data;
+};
+
+// 7.4 언어 요약 조회하기
+export const getLanguagesSummaries = async () => {
+  const response = await api.get('/users/resumes/languages/summaries');
   return response.data;
 };
 
@@ -41,6 +41,32 @@ export const postWorkExperience = async (
     `/users/resumes/work-experiences`,
     workExperience,
   );
+  return response.data;
+};
+
+// 7.6 학력 생성하기
+export const postEducation = async ({
+  id,
+  education,
+}: {
+  id: number;
+  education: EducationRequest;
+}) => {
+  const response = await api.post(`/users/resumes/educations/${id}`, education);
+  return response.data;
+};
+
+// 7.7 언어 - ETC 생성하기
+export const postEtcLanguageLevel = async () => {
+  const response = await api.post(
+    '/users/resumes/languages/additional-languages',
+  );
+  return response.data;
+};
+
+// 7.8 (유학생) 자기소개 수정하기
+export const patchIntroduction = async (introduction: IntroDuctionRequest) => {
+  const response = await api.patch('/users/resumes/introduction', introduction);
   return response.data;
 };
 
@@ -59,30 +85,6 @@ export const patchWorkExperience = async ({
   return response.data;
 };
 
-// 7.16 (유학생) 경력 삭제하기
-export const deleteWorkExperience = async (id: number) => {
-  const response = await api.delete(`/users/resumes/work-experiences/${id}`);
-  return response.data;
-};
-
-// 7.3 학력 상세 조회하기
-export const getEducation = async (id: number) => {
-  const response = await api.get(`/users/resumes/educations/${id}/details`);
-  return response.data;
-};
-
-// 7.6 학력 생성하기
-export const postEducation = async ({
-  id,
-  education,
-}: {
-  id: number;
-  education: EducationRequest;
-}) => {
-  const response = await api.post(`/users/resumes/educations/${id}`, education);
-  return response.data;
-};
-
 // 7.10 (유학생) 학력 수정하기
 export const patchEducation = async ({
   id,
@@ -95,18 +97,6 @@ export const patchEducation = async ({
     `/users/resumes/educations/${id}`,
     education,
   );
-  return response.data;
-};
-
-// 7.17 (유학생) 학력 삭제하기
-export const deleteEducation = async (id: number) => {
-  const response = await api.delete(`/users/resumes/educations/${id}`);
-  return response.data;
-};
-
-// 7.4 언어 요약 조회하기
-export const getLanguagesSummaries = async () => {
-  const response = await api.get('/users/resumes/languages/summaries');
   return response.data;
 };
 
@@ -126,14 +116,6 @@ export const patchLanguagesLevel = async ({
   return response.data;
 };
 
-// 7.7 언어 - ETC 생성하기
-export const postEtcLanguageLevel = async () => {
-  const response = await api.post(
-    '/users/resumes/languages/additional-languages',
-  );
-  return response.data;
-};
-
 // 7.14 (유학생) 언어 - ETC 수정하기
 export const patchaEtcLanguageLevel = async ({
   id,
@@ -149,10 +131,36 @@ export const patchaEtcLanguageLevel = async ({
   return response.data;
 };
 
+// 7.15 (유학생) 자기소개 삭제하기
+export const deleteIntroduction = async () => {
+  const response = await api.delete('/users/resumes/introduction');
+  return response.data;
+};
+
+// 7.16 (유학생) 경력 삭제하기
+export const deleteWorkExperience = async (id: number) => {
+  const response = await api.delete(`/users/resumes/work-experiences/${id}`);
+  return response.data;
+};
+
+// 7.17 (유학생) 학력 삭제하기
+export const deleteEducation = async (id: number) => {
+  const response = await api.delete(`/users/resumes/educations/${id}`);
+  return response.data;
+};
+
 // 7.18 (유학생) 언어 - ETC 삭제하기
 export const deleteEtcLanguageLevel = async (id: number) => {
   const response = await api.delete(
     `/users/resumes/languages/additional-languages/${id}`,
+  );
+  return response.data;
+};
+
+// 7.19 (고용주) 이력서 조회하기
+export const getApplicantResume = async (id: number) => {
+  const response = await api.get(
+    `/owners/user-owner-job-postings/${id}/users/resumes/details`,
   );
   return response.data;
 };
