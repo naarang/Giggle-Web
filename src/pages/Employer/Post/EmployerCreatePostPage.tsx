04a4a7088b1ee1ad664ffde4e9d18beb6d19e8e1
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-30 01:53:46 +0900

    :sparkles: feat: funnel 패턴 단계 구조 완성 #68

--- src/pages/Employer/Post/EmployerCreatePostPage.tsx
+++ src/pages/Employer/Post/EmployerCreatePostPage.tsx
@@ -1,6 +1,10 @@
 import CompleteModal from '@/components/Common/CompleteModal';
 import BaseHeader from '@/components/Common/Header/BaseHeader';
 import Step1 from '@/components/Employer/PostCreate/Step1';
+import Step2 from '@/components/Employer/PostCreate/Step2';
+import Step3 from '@/components/Employer/PostCreate/Step3';
+import Step4 from '@/components/Employer/PostCreate/Step4';
+import Step5 from '@/components/Employer/PostCreate/Step5';
 import StepIndicator from '@/components/Information/StepIndicator';
 import { useCreatePost } from '@/hooks/api/usePost';
 import {
@@ -33,7 +37,7 @@ const EmployerCreatePostPage = () => {
   };
   return (
     <div>
-      <BaseHeader hasBackButton hasMenuButton title="공고등록" />
+      <BaseHeader hasBackButton hasMenuButton={false} title="공고등록" />
       {devIsModal ? (
         <CompleteModal
           title="공고 등록이 완료되었습니다."
@@ -43,10 +47,7 @@ const EmployerCreatePostPage = () => {
       ) : (
         <>
           <div className="w-full flex flex-row p-6 items-center justify-between">
-            <div
-              className="relative w-full flex items-center justify-start title-1 text-[#1e1926] text-left"
-              onClick={() => setCurrentStep(currentStep + 1)}
-            >
+            <div className="relative w-full flex items-center justify-start title-1 text-[#1e1926] text-left">
               공고등록
             </div>
             <StepIndicator
@@ -58,6 +59,35 @@ const EmployerCreatePostPage = () => {
           <div className="w-full flex justify-center px-6">
             {currentStep === 1 && (
               <Step1 postInfo={postInfo} onNext={handleNext} />
+            )}
+            {currentStep === 2 && (
+              <Step2
+                postInfo={postInfo}
+                onNext={handleNext}
+                onPrev={() => setCurrentStep((prev) => prev - 1)}
+              />
+            )}
+            {currentStep === 3 && (
+              <Step3
+                postInfo={postInfo}
+                onNext={handleNext}
+                onPrev={() => setCurrentStep((prev) => prev - 1)}
+              />
+            )}
+            {currentStep === 4 && (
+              <Step4
+                postInfo={postInfo}
+                onNext={handleNext}
+                onPrev={() => setCurrentStep((prev) => prev - 1)}
+              />
+            )}
+            {currentStep === 5 && (
+              <Step5
+                postInfo={postInfo}
+                onNext={handleNext}
+                onSubmit={handleSubmit}
+                onPrev={() => setCurrentStep((prev) => prev - 1)}
+              />
             )}
           </div>
         </>
