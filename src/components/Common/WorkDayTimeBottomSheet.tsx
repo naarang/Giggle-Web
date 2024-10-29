866e63f849ba578571a87ba3ee2e72af593ed2c0
nakyong82 <nakyong7770@gmail.com>
2024-10-30 00:46:21 +0900

    ✨feat: 근로일 및 근로일별 근로시간 #66

---
+++ src/components/Common/WorkDayTimeBottomSheet.tsx
@@ -0,0 +1,164 @@
+import BottomSheetLayout from '@/components/Common/BottomSheetLayout';
+import Button from '@/components/Common/Button';
+import { buttonTypeKeys } from '@/constants/components';
+import { useState } from 'react';
+import TimePicker from '@/components/Common/TimePicker';
+
+// TODO: 나중에 constant로 분리해주세요!
+const DAYS = {
+  ['월요일']: 'MONDAY',
+  ['화요일']: 'TUESDAY',
+  ['수요일']: 'WEDNESDAY',
+  ['목요일']: 'THURSDAY',
+  ['금요일']: 'FRIDAY',
+  ['토요일']: 'SATURDAY',
+  ['일요일']: 'SUNDAY',
+} as const;
+
+type DayType = (typeof DAYS)[keyof typeof DAYS];
+
+// api에게 보낼 데이터 형식
+type WorkDayTimeItemType = {
+  day_of_week: DayType | 'NEGOTIABLE';
+  work_start_time: string | null;
+  work_end_time: string | null;
+};
+
+const WorkDayTimeBottomSheet = () => {
+  const [isShowBottomsheet, setisShowBottomsheet] = useState<boolean>(true);
+
+  const [isCheckAllWeek, setIsCheckAllWeek] = useState<boolean>(false);
+  const [isCheckAllTime, setIsCheckAllTime] = useState<boolean>(false);
+
+  const [dayOfWeek, setDayOfWeek] = useState<DayType[]>([]);
+  const [workStartTime, setWorkStartTime] = useState<string | null>(null);
+  const [workEndTime, setWorkEndTime] = useState<string | null>(null);
+
+  const onClickDayOfWeek = (day: DayType) => {
+    if (dayOfWeek.includes(day)) {
+      setDayOfWeek([...dayOfWeek.filter((value) => value !== day)]);
+    } else {
+      setDayOfWeek([...dayOfWeek, day]);
+      setIsCheckAllWeek(false);
+    }
+  };
+
+  const onClickCheckAllWeek = () => {
+    if (!isCheckAllWeek) setDayOfWeek([]);
+    setIsCheckAllWeek(!isCheckAllWeek);
+  };
+
+  const isAvailableSubmit = () => {
+    if (
+      (dayOfWeek.length || isCheckAllWeek) &&
+      ((workStartTime && workEndTime) || isCheckAllTime)
+    )
+      return true;
+    return false;
+  };
+
+  const onClickSubmit = () => {
+    if (!isAvailableSubmit()) return;
+
+    if (isCheckAllWeek) {
+      const result: WorkDayTimeItemType[] = [
+        {
+          day_of_week: 'NEGOTIABLE',
+          work_start_time: isCheckAllTime ? null : workStartTime,
+          work_end_time: isCheckAllTime ? null : workEndTime,
+        },
+      ];
+
+      console.log(result); // TODO: API에 보낼 형식 맞춰놨쓔!
+    } else {
+      const result: WorkDayTimeItemType[] = dayOfWeek.map((day) => {
+        return {
+          day_of_week: DAYS[day as keyof typeof DAYS],
+          work_start_time: isCheckAllTime ? null : workStartTime,
+          work_end_time: isCheckAllTime ? null : workEndTime,
+        };
+      });
+
+      console.log(result); // TODO: API에 보낼 형식 맞춰놨쓔!
+    }
+    setisShowBottomsheet(false);
+  };
+
+  return (
+    <BottomSheetLayout
+      hasHandlebar={true}
+      isAvailableHidden={true}
+      isShowBottomsheet={isShowBottomsheet}
+    >
+      <div className="w-full">
+        <div className="w-full py-[0.75rem] px-[3.125rem] flex flex-col items-center gap-[0.75rem]">
+          <h3 className="head-2 text-[#1E1926]">근로일 및 근로일별 근로시간</h3>
+          <p className="body-3 text-[#656565]">
+            원하는 근무 시간을 추가해주세요.
+          </p>
+        </div>
+        <div className="w-full mb-[1rem] px-[1.5rem] flex flex-col gap-[0.5rem]">
+          <div>
+            <div className="w-full flex justify-between items-center">
+              <h5 className="px-[0.25rem] py-[0.375rem] text-[#1E1926] body-3">
+                근무일자 <span className="text-[#EE4700]">*</span>
+              </h5>
+              <div className="flex gap-[0.5rem] items-center py-[0.25rem]">
+                <button
+                  className={`w-[1rem] h-[1rem] border  border-[#F4F4F9] ${isCheckAllWeek && 'bg-[#FEF387]'}`}
+                  onClick={onClickCheckAllWeek}
+                ></button>
+                <p className="body-3 text-[#656565]">요일무관</p>
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-[0.5rem] w-full">
+              {Object.keys(DAYS).map((value, index) => (
+                <button
+                  className={`py-[0.375rem] px-[0.875rem] body-3 border border-[#EFEFEF] rounded-[1.125rem] ${dayOfWeek.includes(value as DayType) ? 'bg-[#FEF387]' : 'bg-white'}`}
+                  key={`${value}_${index}`}
+                  onClick={() => onClickDayOfWeek(value as DayType)}
+                >
+                  {value}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div>
+            <div className="w-full flex justify-between items-center">
+              <h5 className="px-[0.25rem] py-[0.375rem] text-[#1E1926] body-3">
+                근무시간 <span className="text-[#EE4700]">*</span>
+              </h5>
+              <div className="flex gap-[0.5rem] items-center py-[0.25rem]">
+                <button
+                  className={`w-[1rem] h-[1rem] border  border-[#F4F4F9] ${isCheckAllTime && 'bg-[#FEF387]'}`}
+                  onClick={() => setIsCheckAllTime(!isCheckAllTime)}
+                ></button>
+                <p className="body-3 text-[#656565]">시간무관</p>
+              </div>
+            </div>
+            <div className="flex gap-[0.25rem] w-full ">
+              <TimePicker
+                isDisabled={!dayOfWeek.length && isCheckAllTime}
+                onChangeTime={setWorkStartTime}
+              />
+              <TimePicker
+                isDisabled={!dayOfWeek.length && isCheckAllTime}
+                onChangeTime={setWorkEndTime}
+              />
+            </div>
+          </div>
+        </div>
+        <Button
+          type={buttonTypeKeys.LARGE}
+          bgColor={isAvailableSubmit() ? `bg-[#FEF387]` : `bg-[#F4F4F9]`}
+          fontColor={isAvailableSubmit() ? `text-[#1E1926]` : `text-[#BDBDBD]`}
+          isBorder={false}
+          title={'추가하기'}
+          onClick={onClickSubmit}
+        />
+      </div>
+    </BottomSheetLayout>
+  );
+};
+
+export default WorkDayTimeBottomSheet;
