6e0e248d7832c1d99370064a629570b18b7ad8f9
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-01 02:10:40 +0900

    ✨ feat: 고용주 프로필 조회 api 연결 #73

--- src/components/Employer/Profile/EmployerJobInfo.tsx
+++ src/components/Employer/Profile/EmployerJobInfo.tsx
@@ -1,30 +1,37 @@
-import { EmployerInfoData } from '@/constants/profile';
-// import { useGetOwnerApplicationCounts } from '@/hooks/api/useProfile';
+import { useGetOwnerApplicationCounts } from '@/hooks/api/useProfile';
+import { EmployerCountsInfoResponse } from '@/types/api/profile';
+import { useEffect, useState } from 'react';
 
 const EmployerJobInfo = () => {
-  // const { data } = useGetOwnerApplicationCounts();
-  const data = EmployerInfoData;
+  const { data } = useGetOwnerApplicationCounts();
+  const [jobData, setJobData] = useState<EmployerCountsInfoResponse>();
+
+  useEffect(() => {
+    if (data) {
+      setJobData(data.data);
+    }
+  }, [data]);
 
   return (
     <>
-      {data && data.success ? (
+      {jobData ? (
         <div className="flex gap-4 items-center justify-between">
           <div className="flex flex-col gap-3 w-24 py-2 px-1">
             <div className="body-3 text-[#1E1926] text-center">올린 공고</div>
             <div className="head-3 text-[#1E1926] text-center">
-              {data.data.job_postings_counts}
+              {jobData.job_postings_counts}
             </div>
           </div>
           <div className="flex flex-col gap-3 py-2 px-1">
             <div className="body-3 text-[#1E1926] text-center">지원자 총합</div>
             <div className="head-3 text-[#1E1926] text-center">
-              {data.data.applicants_counts}
+              {jobData.applicants_counts}
             </div>
           </div>
           <div className="flex flex-col gap-3 w-24 py-2 px-1">
             <div className="body-3 text-[#1E1926] text-center">계약 성공</div>
             <div className="head-3 text-[#1E1926] text-center">
-              {data.data.successful_hire_counts}
+              {jobData.successful_hire_counts}
             </div>
           </div>
         </div>
