14d531778cba98aa4b093de1fa47ec58d0ea7e87
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-01 05:52:18 +0900

    🐛 fix: 고용주 프로필 API 수정 #73

--- src/components/Employer/Profile/EmployerProfileMenuList.tsx
+++ src/components/Employer/Profile/EmployerProfileMenuList.tsx
@@ -1,6 +1,6 @@
-import ProfileMenu from '@/components/Profile/ProfileMenu';
 import { IconType } from '@/constants/profile';
 import { useNavigate } from 'react-router-dom';
+import EmployerProfileMenu from './EmployerProfileMenu';
 
 type EmployerProfileMenuListProps = {
   handleBottomSheetOpen: () => void;
@@ -17,17 +17,17 @@ const EmployerProfileMenuList = ({
 
   return (
     <div className="flex flex-col gap-9 pt-9">
-      <ProfileMenu
+      <EmployerProfileMenu
         title="회사/점포 정보 수정"
         iconType={IconType.PROFILE}
         onClick={handleEditProfileClick}
       />
-      <ProfileMenu
+      <EmployerProfileMenu
         title="알림 수신"
         iconType={IconType.NOTIFICATION}
         isToggle={true}
       />
-      <ProfileMenu
+      <EmployerProfileMenu
         title="로그아웃"
         iconType={IconType.LOGOUT}
         onClick={handleBottomSheetOpen}
