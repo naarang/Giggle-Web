f58532e599c515c59d584f85f9c378b4fa80d43b
EunJiJung <bianbbc87@gmail.com>
2024-11-01 12:05:19 +0900

    ♻️ refactor: 고용주 메인 한글로 변경
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/components/Home/HomeEmployerBanner.tsx
+++ src/components/Home/HomeEmployerBanner.tsx
@@ -7,9 +7,9 @@ const HomeEmployerBanner = () => {
     <section className="w-full px-[1.5rem] pb-[2.75rem] bg-[#FEF387]">
       <div className="flex flex-col justify-center items-center gap-[0.5rem] p-[1.5rem] rounded-[1.5rem] bg-cover bg-center bg-[url('/src/assets/images/yellowGradient.png')]">
         <h3 className="head-3 text-[#1E1926] text-center">
-          Register your
+          새로운 공고를
           <br />
-          new announcement
+          등록해보세요.
         </h3>
         {/* TODO: 공고 작성 페이지로 이동 필요 */}
         <button
