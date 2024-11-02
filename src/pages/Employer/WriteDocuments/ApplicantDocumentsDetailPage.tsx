1807bd6099ae900cbceb52f1b420679d84f84127
EunJiJung <bianbbc87@gmail.com>
2024-11-02 17:15:22 +0900

    🐛 fix: 서류 수정 페이지로 이동 시 id 값 매핑
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/pages/Employer/WriteDocuments/ApplicantDocumentsDetailPage.tsx
+++ src/pages/Employer/WriteDocuments/ApplicantDocumentsDetailPage.tsx
@@ -51,7 +51,7 @@ const ApplicantDocumentsDetailPage = () => {
             title={DocumentTypeInfo[DocumentType.PART_TIME_PERMIT].name}
             type={DocumentType.PART_TIME_PERMIT}
             reason={data?.data[DocumentType.PART_TIME_PERMIT].reason || undefined}
-            onNext={() => handleOnNext(Number(id))}
+            onNext={() => handleOnNext(data.data[DocumentType.PART_TIME_PERMIT]?.id || 0)}
           />
         ) : (
           <div className="w-full relative rounded-[1.125rem] bg-white border border-[#dcdcdc] flex flex-col items-start justify-start py-6 cursor-pointer text-left text-[#1e1926]">
@@ -69,7 +69,8 @@ const ApplicantDocumentsDetailPage = () => {
             document={data.data[DocumentType.LABOR_CONTRACT] as EmployDocumentInfo}
             title={DocumentTypeInfo[DocumentType.LABOR_CONTRACT].name}
             type={DocumentType.LABOR_CONTRACT}
-            onNext={() => {}}
+            reason={data?.data[DocumentType.LABOR_CONTRACT].reason || undefined}
+            onNext={() => handleOnNext(data.data[DocumentType.LABOR_CONTRACT]?.id || 0)}
           />
         ) : (
           <div className="w-full relative rounded-[1.125rem] bg-white border border-[#dcdcdc] flex flex-col items-start justify-start py-6 cursor-pointer text-left text-[#1e1926]">
