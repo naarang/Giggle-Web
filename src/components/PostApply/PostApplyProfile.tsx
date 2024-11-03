307b6aa7963db090eaa23987e553a7480f6979c6
nakyong82 <nakyong7770@gmail.com>
2024-11-04 00:47:39 +0900

    🐛 임시 이미지 제거하고 필요한 이미지 넣기 #103

--- src/components/PostApply/PostApplyProfile.tsx
+++ src/components/PostApply/PostApplyProfile.tsx
@@ -1,12 +1,21 @@
 type PostApplyProfileProps = {
-  profileImgUrl: string;
+  profileImgUrl: string | undefined;
   name: string;
 };
 
-const PostApplyProfile = ({ name }: PostApplyProfileProps) => {
+const PostApplyProfile = ({ profileImgUrl, name }: PostApplyProfileProps) => {
   return (
     <div>
-      <div className='w-[5.125rem] h-[5.125rem] mb-[1rem] rounded-full bg-cover bg-center bg-[url("/src/assets/images/JobIconExample.jpeg")]'></div>
+      {profileImgUrl ? (
+        <div
+          className="w-[5.125rem] h-[5.125rem] mb-[1rem] rounded-full bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${profileImgUrl})`,
+          }}
+        ></div>
+      ) : (
+        <div className="w-[5.125rem] h-[5.125rem] mb-[1rem] rounded-full bg-[#F4F4F9]]"></div>
+      )}
       <p className=" text-[#33384B] font-bold text-lg">{name}</p>
     </div>
   );
