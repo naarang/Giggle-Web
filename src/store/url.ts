47ff9d485d7b9cdb4bf59ef87433b9efe672a8f5
nakyong82 <nakyong7770@gmail.com>
2024-11-05 00:39:23 +0900

    🐛 fix: zustand persist 적용하기 #103

--- src/store/url.ts
+++ src/store/url.ts
@@ -1,21 +1,38 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type CurrentPostIdStore = {
   currentPostId: number | null;
   updateCurrentPostId: (id: number) => void;
 };
 
-export const useCurrentPostIdStore = create<CurrentPostIdStore>()((set) => ({
-  currentPostId: null,
-  updateCurrentPostId: (newId: number) => set(() => ({ currentPostId: newId })),
-}));
+export const useCurrentPostIdStore = create(
+  persist<CurrentPostIdStore>(
+    (set) => ({
+      currentPostId: null,
+      updateCurrentPostId: (newId: number) =>
+        set(() => ({ currentPostId: newId })),
+    }),
+    {
+      name: 'currentPostIdStore',
+    },
+  ),
+);
 
 type CurrentApplicantIdStore = {
   currentApplicantId: number | null;
   updateCurrentApplicantId: (id: number) => void;
 };
 
-export const useCurrentApplicantIdStore = create<CurrentApplicantIdStore>()((set) => ({
-  currentApplicantId: null,
-  updateCurrentApplicantId: (newId: number) => set(() => ({ currentApplicantId: newId })),
-}));
+export const useCurrentApplicantIdStore = create(
+  persist<CurrentApplicantIdStore>(
+    (set) => ({
+      currentApplicantId: null,
+      updateCurrentApplicantId: (newId: number) =>
+        set(() => ({ currentApplicantId: newId })),
+    }),
+    {
+      name: 'currentApplicantIdStore',
+    },
+  ),
+);
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,5 +1,6 @@
 import { UserType } from '@/constants/user';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type UserStore = {
   account_type: UserType | undefined; // 계정 유형(유학생, 고용주)
@@ -8,9 +9,16 @@ type UserStore = {
   updateName: (name: string) => void; // 이름 업데이트 함수
 };
 
-export const useUserStore = create<UserStore>()((set) => ({
-  account_type: undefined,
-  name: '',
-  updateAccountType: (account_type) => set(() => ({ account_type })),
-  updateName: (name) => set(() => ({ name })),
-}));
+export const useUserStore = create(
+  persist<UserStore>(
+    (set) => ({
+      account_type: undefined,
+      name: '',
+      updateAccountType: (account_type) => set(() => ({ account_type })),
+      updateName: (name) => set(() => ({ name })),
+    }),
+    {
+      name: 'userStore',
+    },
+  ),
+);
