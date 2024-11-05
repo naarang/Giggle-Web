ba7f9ff823fb87ab51e4ba6916b954830339eb9d
nakyong82 <nakyong7770@gmail.com>
2024-11-05 23:12:36 +0900

    ✨ feat: 알람 목록 조회하기 무한 스크롤 적용하기 #103

--- src/hooks/api/useAlarm.ts
+++ src/hooks/api/useAlarm.ts
@@ -1,5 +1,5 @@
 import { getAlarms, patchReadAlarm } from '@/api/alarm';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query';
 
 // 10.1 (유학생/고용주) 알림 조회 훅
 export const useGetAlarms = (
@@ -8,10 +8,31 @@ export const useGetAlarms = (
   isEnabled: boolean,
 ) => {
   return useQuery({
-    queryKey: ['alarm'],
+    queryKey: ['alarm', page, size],
     queryFn: () => getAlarms(page, size),
     enabled: isEnabled,
   });
+};
+
+export const useInfiniteGetAlarms = (size: number) => {
+  const { data, isLoading, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ['alarm', size],
+      queryFn: ({ pageParam = 1 }) => getAlarms(pageParam, size),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPage) => {
+        return lastPage.data.has_next ? allPage.length + 1 : undefined;
+      },
+      retry: 1,
+    });
+
+  return {
+    data,
+    isLoading,
+    fetchNextPage,
+    hasNextPage: data?.pages[data?.pages.length - 1].data.has_next,
+    isFetchingNextPage,
+  };
 };
 
 // 10.2 (유학생/고용주) 알림 읽음 상태 변경 훅
