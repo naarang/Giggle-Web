a955fd6765ac2de947600d6abad9ef5ee54e3c3a
naarang <nakyong07@naver.com>
2024-11-05 18:05:42 +0000

    🐛 fix: 프로필 사진만 변경해도 수정 가능 #23

--- src/components/Splash/Splash.tsx
+++ src/components/Splash/Splash.tsx
@@ -2,42 +2,62 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import SplashIcon from '@/assets/icons/Splash.svg?react';
 import { getAccessToken, getRefreshToken } from '@/utils/auth';
-import { useGetUserType, useReIssueToken } from '@/hooks/api/useAuth';
+import {
+  useGetUserType,
+  useReIssueToken,
+  useSignIn,
+} from '@/hooks/api/useAuth';
 import { useUserStore } from '@/store/user';
+import { useUserInfoforSigninStore } from '@/store/signup';
 
 const Splash = () => {
   const navigate = useNavigate();
   const { updateAccountType, updateName } = useUserStore();
+  const { id, password } = useUserInfoforSigninStore();
+
   const { data: userTypeResponse } = useGetUserType();
   const { mutate: reIssueToken } = useReIssueToken();
+  const { mutate: signin } = useSignIn();
 
+  // 회원가입 직후 이동된 user의 경우 signin 수행
+  useEffect(() => {
+    if (id && password) {
+      console.log('스플래시 id, password 존재');
+
+      const signinData = new FormData();
+      signinData.append('serial_id', id);
+      signinData.append('password', password);
+
+      signin(signinData);
+    }
+  }, [id, password]);
+
+  // 로그인/로그아웃/탈퇴시 유저 상태 확인
   useEffect(() => {
     const handleApiCall = async () => {
       const access = getAccessToken();
       const refresh = getRefreshToken();
 
       try {
-        // 1. access, refresh 모두 없는 경우
         if (!access && !refresh) {
           setGuest();
-          return;
         }
 
-        // 2. access, refresh 모두 있는 경우
+        if (!access && refresh) {
+          if (userTypeResponse?.error?.code === 401) {
+            reIssueToken();
+          } else {
+            setGuest();
+          }
+        }
+
         if (access && refresh) {
-          // 2-1. user type 응답 성공
           if (userTypeResponse?.success) {
             const { account_type, name } = userTypeResponse.data;
             updateAccountType(account_type);
             updateName(name);
-          }
-          // 2-2. access 토큰이 만료되었을 경우(401)
-          else if (userTypeResponse?.error?.code === 401) {
-            // 2-2-1. API 호출 - JWT 재발급
-            reIssueToken();
-          }
-          // 2-3. user type 응답 실패
-          else {
+            console.log('유저 정보 업데이트');
+          } else {
             setGuest();
           }
         }
@@ -45,7 +65,7 @@ const Splash = () => {
         alert('로그인 오류입니다 다시 시도해주세요');
         setGuest();
       } finally {
-        // 스플래시 화면 2초간 유지
+        // 스플래시 화면 2초간 유지 후 홈으로 이동
         await new Promise((resolve) => setTimeout(resolve, 2000));
         navigate('/');
       }
