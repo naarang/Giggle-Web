72152edf17835b1592fd86ba2d944ab4aec142a4
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-11-06 01:00:38 +0900

    :bug: fix: 통합신청서 preview 안보이던 현상 수정 #105

--- src/components/Document/write/IntegratedApplicationPreview.tsx
+++ src/components/Document/write/IntegratedApplicationPreview.tsx
@@ -190,7 +190,7 @@ const IntegratedApplicationPreview = ({
             <div className="border border-gray-200 rounded-xl">
               {document?.signature_base64 !== '' && (
                 <img
-                  src={`data:image/${getImageType(document?.signature_base64 as string)};base64,${document?.signature_base64}`}
+                  src={`data:image/svg+xml;base64,${document?.signature_base64}`}
                   className="w-full h-full object-cover"
                   alt="signature preview"
                 />
