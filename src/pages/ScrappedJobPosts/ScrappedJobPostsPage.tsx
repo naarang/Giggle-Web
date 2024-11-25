02dde3c0417560b85d9faa398357d855f23a8466
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-26 00:45:13 +0900

    🐛 fix: 스크랩 공고 렌더링 에러 해결 #109

--- src/pages/ScrappedJobPosts/ScrappedJobPostsPage.tsx
+++ src/pages/ScrappedJobPosts/ScrappedJobPostsPage.tsx
@@ -7,45 +7,38 @@ import { useNavigate } from 'react-router-dom';
 
 const ScrappedJobPostsPage = () => {
   const navigate = useNavigate();
-  const [jobPostingData, setJobPostingData] =
-    useState<ScrappedJobPostingType[]>();
+  const [jobPostingData, setJobPostingData] = useState<
+    ScrappedJobPostingType[]
+  >([]);
 
   // TODO: 무한스크롤 구현
   const { data } = useGetBookmarkPostList(1, 20);
 
   useEffect(() => {
     // API - 5.1 (유학생) 북마크한 공고 리스트 조회하
-    if (data) {
-      setJobPostingData(data.data.job_posting_list);
-
+    if (data?.data?.job_posting_list) {
       const updatedJobPostingData = data.data.job_posting_list.map(
         (item: ScrappedJobPostingType) => ({
           ...item,
           is_book_marked: true,
         }),
       );
-
-      // 상태 업데이트
       setJobPostingData(updatedJobPostingData);
     }
   }, [data]);
   return (
     <>
-      {jobPostingData && (
-        <div>
-          <BaseHeader
-            hasBackButton={true}
-            onClickBackButton={() => navigate('/profile')}
-            hasMenuButton={false}
-            title="Scrap Job Posting"
-          />
-          <div className="p-6 flex flex-col gap-4">
-            {jobPostingData.map((post) => (
-              <JobPostingCard jobPostingData={post} />
-            ))}
-          </div>
-        </div>
-      )}
+      <BaseHeader
+        hasBackButton
+        onClickBackButton={() => navigate('/profile')}
+        hasMenuButton={false}
+        title="Scrap Job Posting"
+      />
+      <div className="p-6 flex flex-col gap-4">
+        {jobPostingData.map((post) => (
+          <JobPostingCard key={post.id} jobPostingData={post} />
+        ))}
+      </div>
     </>
   );
 };
