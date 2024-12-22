007db11d137a740ebe915b4c074e6f499747ab4b
naarang <nakyong07@naver.com>
2024-12-22 14:23:06 +0000

    🐛 fix: 회원가입 이메일 인증 코드 입력방식 숫자로 변경 #134

--- src/components/ManageResume/components/EducationDetail.tsx
+++ src/components/ManageResume/components/EducationDetail.tsx
@@ -22,20 +22,9 @@ const EducationDetail = ({ data }: EducationDetailProps) => {
       {data.map((education) => (
         <div
           key={education.id}
-          className="relative px-3 py-4 flex justify-between items-start bg-[#F4F4F9] rounded-xl"
+          className="relative px-3 py-4 flex items-start bg-[#F4F4F9] rounded-xl"
         >
-          <div className="text-[#656565]">
-            <p className="head-3 mb-1">{education.education_level}</p>
-            <div className="flex gap-1 body-3 mb-4 mr-4 justify-between">
-              <p className="max-w-[50%]">{education.school_name}</p>
-              <p>•</p>
-              <p className="max-w-[50%]">{education.major}</p>
-            </div>
-            <p className="caption-1">
-              {formatDate(education.start_date)} ~{' '}
-              {formatDate(education.end_date)}
-            </p>
-          </div>
+          {/* 수정, 삭제 아이콘 */}
           <div className="absolute top-4 right-3 flex justify-center items-center gap-2.5 ml-1">
             <EditIcon
               onClick={() => navigate(`/resume/education/edit/${education.id}`)}
@@ -45,6 +34,19 @@ const EducationDetail = ({ data }: EducationDetailProps) => {
               onClick={() => handleDelete(education.id)}
               className="cursor-pointer"
             />
+          </div>
+          {/* 학력 정보 */}
+          <div className="text-[#656565]">
+            <p className="head-3 mb-1">{education.education_level}</p>
+            <div className="flex gap-1 body-3 mb-4 mr-4">
+              <p>{education.school_name}</p>
+              <p>•</p>
+              <p>{education.major}</p>
+            </div>
+            <p className="caption-1">
+              {formatDate(education.start_date)} ~{' '}
+              {formatDate(education.end_date)}
+            </p>
           </div>
         </div>
       ))}
