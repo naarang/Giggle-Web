f41b95ff1864a95c10d05398168bf6887ce9fee1
naarang <nakyong07@naver.com>
2025-02-11 06:45:37 +0000

    ✨ feat: 회원가입 중 step1 내 생년월일, 국적 optional 전환

--- src/components/Employer/Profile/EmployerProfileMenuList.tsx
+++ src/components/Employer/Profile/EmployerProfileMenuList.tsx
@@ -10,28 +10,38 @@ const EmployerProfileMenuList = ({
   handleBottomSheetOpen,
 }: EmployerProfileMenuListProps) => {
   const navigate = useNavigate();
-
-  const handleEditProfileClick = () => {
-    navigate('/employer/profile/edit');
+  const handleAccountClick = () => {
+    navigate('/profile/account');
+  };
+  const handleAboutClick = () => {
+    navigate('/profile/about');
   };
 
   return (
-    <div className="flex flex-col gap-9 pt-9">
-      <EmployerProfileMenu
-        title="회사/점포 정보 수정"
-        iconType={IconType.PROFILE}
-        onClick={handleEditProfileClick}
-      />
-      <EmployerProfileMenu
-        title="알림 수신"
-        iconType={IconType.NOTIFICATION}
-        isToggle={true}
-      />
-      <EmployerProfileMenu
-        title="로그아웃"
-        iconType={IconType.LOGOUT}
-        onClick={handleBottomSheetOpen}
-      />
+    <div className="flex flex-col gap-4 px-4 pb-4 bg-white rounded-md">
+      <div className="flex flex-col divide-y divide-gray-200">
+        <EmployerProfileMenu
+          title="계정"
+          iconType={IconType.PROFILE}
+          onClick={handleAccountClick}
+        />
+        <EmployerProfileMenu
+          title="알림 수신"
+          iconType={IconType.NOTIFICATION}
+          isToggle={true}
+        />
+        <EmployerProfileMenu
+          title="정보"
+          iconType={IconType.PROFILE}
+          onClick={handleAboutClick}
+        />
+        <button
+          className="grow w-full bg-[#ebeef1] rounded-md px-5 py-3 text-center button-2 text-[#1E1926]"
+          onClick={handleBottomSheetOpen}
+        >
+          로그아웃
+        </button>
+      </div>
     </div>
   );
 };
---
+++ src/pages/Profile/AccountPage.tsx
@@ -0,0 +1,57 @@
+import BaseHeader from '@/components/Common/Header/BaseHeader';
+import DeleteModal from '@/components/Profile/DeleteModal';
+import ProfileMenu from '@/components/Profile/ProfileMenu';
+import { IconType } from '@/constants/profile';
+import { profileTranslation } from '@/constants/translation';
+import { useUserStore } from '@/store/user';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserType } from '@/constants/user';
+
+const AccountPage = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const { account_type } = useUserStore();
+  const navigate = useNavigate();
+  // 계정 삭제 모달 핸들러
+  const handleDeleteButton = (value: boolean) => {
+    setModalOpen(value);
+  };
+
+  return (
+    <>
+      {/* 계정 삭제 modal */}
+      {modalOpen && (
+        <DeleteModal
+          onDeleteButton={handleDeleteButton} // 계정 삭제 취소 버튼
+        />
+      )}
+      <BaseHeader
+        hasBackButton
+        onClickBackButton={() =>
+          navigate(
+            account_type === UserType.USER ? '/profile' : '/employer/profile',
+          )
+        }
+        hasMenuButton={false}
+        title={account_type === UserType.USER ? 'Account' : '계정'}
+      />
+      <div className="w-full h-full min-h-[100vh] bg-[#f4f4f9]">
+        <div className="flex flex-col gap-4 px-4 pb-4 bg-white rounded-md">
+          <div className="flex flex-col divide-y divide-gray-200">
+            <ProfileMenu
+              title={
+                account_type === UserType.USER
+                  ? profileTranslation.deleteAccount.en
+                  : profileTranslation.deleteAccount.ko
+              }
+              iconType={IconType.LOGOUT}
+              onClick={() => handleDeleteButton(true)}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AccountPage;
